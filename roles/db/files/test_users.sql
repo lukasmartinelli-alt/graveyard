402f266876b189ac7289a178b23a391dc1df4ca6
Lukas Martinelli <me@lukasmartinelli.ch>
2014-10-02 21:44:17 +0200

    Added manuelroth.ch to mail adresses

--- roles/db/files/test_users.sql
+++ roles/db/files/test_users.sql
@@ -1,7 +1,8 @@
 INSERT INTO domains (domain)
-VALUES ('lukasmartinelli.ch'), ('mailgenic.com');
+VALUES ('lukasmartinelli.ch'), ('mailgenic.com'), ('manuelroth.ch');
 
 -- Password is "test"
 INSERT INTO users (mail, domain, password)
 VALUES ('me@lukasmartinelli.ch', 'lukasmartinelli.ch', '$6$t8yoaEQ1chO2MLoV$.yq6VeDhVWp5cUZk3sif.eQq9/suh504zlHnbb0B5ic/4c6QCwyqqQovyP4Ipe1oBpRgGA09m1bGfrwjgOEsL0'),
-('admin@mailgenic.com', 'mailgenic.com', '$6$t8yoaEQ1chO2MLoV$.yq6VeDhVWp5cUZk3sif.eQq9/suh504zlHnbb0B5ic/4c6QCwyqqQovyP4Ipe1oBpRgGA09m1bGfrwjgOEsL0');
+('admin@mailgenic.com', 'mailgenic.com', '$6$t8yoaEQ1chO2MLoV$.yq6VeDhVWp5cUZk3sif.eQq9/suh504zlHnbb0B5ic/4c6QCwyqqQovyP4Ipe1oBpRgGA09m1bGfrwjgOEsL0'),
+('mail@manuelroth.ch', 'manuelroth.ch', '$6$t8yoaEQ1chO2MLoV$.yq6VeDhVWp5cUZk3sif.eQq9/suh504zlHnbb0B5ic/4c6QCwyqqQovyP4Ipe1oBpRgGA09m1bGfrwjgOEsL0');
