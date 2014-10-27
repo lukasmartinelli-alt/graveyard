49b5fa7bb6ce4ac3a3dc0de96bb57361d169e5e4
Lukas Martinelli <me@lukasmartinelli.ch>
2014-10-27 21:16:03 +0100

    Views check domain availability"

--- registration/forms.py
+++ registration/forms.py
@@ -1,9 +1,9 @@
 from django.forms import Form, CharField, ChoiceField
 
 SUPPORTED_TLDS_CHOICES = (
-    ("ch", ".ch"),
-    ("com", ".com"),
-    ("me", ".me"),
+    (".ch", ".ch"),
+    (".com", ".com"),
+    (".me", ".me"),
 )
 
 class RegistrationForm(Form):
--- registration/views.py
+++ registration/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 from .forms import RegistrationForm
+from .whois import check_domain_availability
 
 def register_domain(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
-
-        domain = request.POST["domain"]
-        tld = request.POST["tld"]
-        form = RegistrationForm()
-
-        return render(request, "registration.html", {"form": form})
+        if form.is_valid():
+            domain = form.cleaned_data["domain"] + form.cleaned_data["tld"]
+            availability = check_domain_availability(domain)
+            return render(request, "registration.html", {"domain": domain, "availability": availability})
