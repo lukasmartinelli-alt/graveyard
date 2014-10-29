0efb5bbb906d613c9df417dde4f0fd16d63c829b
Lukas Martinelli <me@lukasmartinelli.ch>
2014-10-29 23:40:55 +0100

    Styled mail adresses and buy button

--- mailgenic/settings.py
+++ mailgenic/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Own apps,
+    # Own apps
     'front',
     'registration',
 )
