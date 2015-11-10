067dd5adb261a5e9d11b6f1a53483c065ef61c24
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-10 20:52:04 +0100

    Started on chapter 5 recursion

--- 5-recursion.hs
+++ 5-recursion.hs
@@ -1,7 +1,9 @@
 -- Raise x to the power y, using recursion
 -- For example, power 5 2 = 25
 power :: Int -> Int -> Int
-power x y = undefined
+power x 0 = 1
+power x 1 = x
+power x y = x * (power x (y-1))
 
 -- create a list of length n of the fibbonaci sequence in reverse order
 -- examples: fib 0 = [0]
@@ -9,7 +11,11 @@ power x y = undefined
 --	     fib 10 = [55,34,21,13,8,5,3,2,1,1,0]	
 -- try to use a where clause
 fib :: (Num a, Eq a) => a -> [a]
-fib x = undefined
+fib 0 = [0]
+fib 1 = [1] ++ fib 0
+fib n = [head prev + head prev2] ++ prev
+        where prev = fib (n-1)
+              prev2 = fib (n-2)
 
 -- This is not recursive, but have a go anyway.
 -- Create a function which takes two parameters, a number and a step
@@ -18,7 +24,7 @@ fib x = undefined
 --			    stepReverseSign -3 1 = 4
 --			    stepReverseSign 1 2 = -3
 stepReverseSign :: (Fractional a, Ord a) => a -> a -> a
-stepReverseSign a = undefined
+stepReverseSign num step = abs(-num) + step
 
 {- Lets calculate pi.
  - The Leibniz formula for pi (http://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80)
