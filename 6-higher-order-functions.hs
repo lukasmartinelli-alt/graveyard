f8194e4ec33378218c1dfedee646bac385fc4e56
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-11 08:35:42 +0100

    Starting on higher order functions

--- 6-higher-order-functions.hs
+++ 6-higher-order-functions.hs
@@ -2,7 +2,10 @@
 -- Example: sumInts 0 1 = 1
 --          sumInts 1 3 = 6
 sumInts :: Int -> Int -> Int
-sumInts a b = undefined
+sumInts a b
+    | a == b    = 0
+    | a > b     = undefined
+    | otherwise = (b - a) + (sumInts a $ b - 1)
 
 -- Define a square function
 sq :: Int -> Int
@@ -10,24 +13,30 @@ sq x = undefined
 
 -- Sum the squares between two numbers. This function should be similar to the sumInts function
 sumSquares :: Int -> Int -> Int
-sumSquares a b = undefined
+sumSquares a b
+    | a == b    = 0
+    | a > b     = undefined
+    | otherwise = (b - a)^2 + (sumSquares a $ b - 1)
 
 -- Define a higher order sum function which accepts an (Int -> Int) function to apply to all integers between two values.
 -- Again this should look similar to the sumInts and sumSquares functions
 higherOrderSum :: (Int -> Int) -> Int -> Int -> Int
-higherOrderSum intApplication a b = undefined
+higherOrderSum intApplication a b
+    | a == b    = 0
+    | a > b     = undefined
+    | otherwise = intApplication (b - a) + (higherOrderSum intApplication a $ b - 1)
 
 -- Define the square sum in terms of higherOrderSum
 hoSumSquares :: Int -> Int -> Int
-hoSumSquares = undefined
+hoSumSquares a b = higherOrderSum (\i -> i^2) a b
 
 -- Define the sum between two values in terms of higherOrderSum
 -- Note there is no parameter on the function definition
 -- Try to use a lambda if possible
 hoSumInts :: Int -> Int -> Int
-hoSumInts = undefined
+hoSumInts a b = higherOrderSum id a b
 
--- Create a new higher order method which generalises over the function provided by sumInts (That is, parameterize (+) :: Int -> Int -> Int) between a and b
+-- Create a new highejr order method which generalises over the function provided by sumInts (That is, parameterize (+) :: Int -> Int -> Int) between a and b
 -- This will give the ability to perform utilities such as the prodcut of all squares (or any other Int -> Int function) between a and b
 -- You will also need to generalise the base case
 -- You can also define the function signature yourself, which leaves you free to define the parameters and their order
