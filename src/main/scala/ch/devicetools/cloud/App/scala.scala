7b2fbfc9850261e9412db3844e581dc2d75d7247
lukasmartinelli <me@lukasmartinelli.ch>
2016-03-12 19:17:08 +0100

    Add simple scala program

---
+++ src/main/scala/ch/devicetools/cloud/App/scala.scala
@@ -0,0 +1,18 @@
+package ch.devicetools.cloud.App
+
+object App {
+
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+
+    val myList = List(1,2,3)
+    println("Length of " + myList + " is: " + listLength(myList))
+  }
+
+  def listLength[A](list: List[A]) : Int = {
+    list match {
+      case Nil => 0
+      case head :: tail => 1 + listLength(tail)
+    }
+  }
+}
