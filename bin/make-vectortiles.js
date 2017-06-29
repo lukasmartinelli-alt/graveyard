65edbf4f36736346505385f984f66d2dea5dd43a
Lukas Martinelli <me@lukasmartinelli.ch>
2017-06-28 21:43:22 -0400

    Basic tile gunzip

--- bin/make-vectortiles.js
+++ bin/make-vectortiles.js
@@ -9,6 +9,7 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const exec = require('child_process').exec;
 const tilelive = require("tilelive");
+const walk = require('walk');
 
 require('tilelive-file').registerProtocols(tilelive);
 require('mbtiles').registerProtocols(tilelive);
@@ -18,13 +19,31 @@ program
   .option('-d, --output-dir [dir]', 'Directrory to store tiles in')
   .parse(process.argv);
 
-function copyTiles(mbtilesPath, outputDir) {
+function copyTiles(mbtilesPath, outputDir, cb) {
 	const source = "mbtiles://" + path.resolve(mbtilesPath);
 	const target = "file://" + path.resolve(outputDir);
   tilelive.copy(source, target, {
     type: 'scanline',
   }, function(err) {
     if (err) throw err;
+		cb();
+  });
+}
+
+function fixTiles(outputDir) {
+  const walker = walk.walk(outputDir, {
+
+	});
+  walker.on("file", function (root, fileStats, next) {
+		if(path.extname(fileStats.name) === '.png') {
+			const newFile = path.join(root, path.basename(fileStats.name, '.png') + '.pbf.gz');
+			fs.renameSync(path.join(root, fileStats.name), newFile);
+			//TODO: No subprocessing required
+			exec(['gunzip', newFile].join(' '), function(err) {
+				if(err) throw err;
+			});
+		}
+		next();
   });
 }
 
@@ -34,7 +53,9 @@ if (program.input && program.outputDir) {
 	const mbtilesPath = path.join(program.outputDir, 'tiles.mbtiles');
 	exec(['tippecanoe -zg -o', mbtilesPath, program.input].join(' '), function(err) {
 		if(err) throw err;
-		copyTiles(mbtilesPath, program.outputDir);
+		copyTiles(mbtilesPath, program.outputDir, function(err) {
+			fixTiles(program.outputDir);
+		});
 	});
 } else {
   program.outputHelp();
